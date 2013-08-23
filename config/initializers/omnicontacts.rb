278d30b04832849674695ceb9554418bace80e25
salayhin <salayhin@gmail.com>
2013-08-23 17:26:37 +0600

    Client ID and secret added

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -2,6 +2,6 @@ require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
   importer :gmail, "819494646461.apps.googleusercontent.com", "56ewhQrGyVDlMFp6TDWlwyJB", {:redirect_path => "/oauth2callback"}
-  importer :yahoo, "dj0yJmk9WEFzREpidHBJM1U5JmQ9WVdrOWR6bEViM0ZrTTJVbWNHbzlPRGswTVRBMU9EWXkmcz1jb25zdW1lcnNlY3JldCZ4PTgw", "c43984c5cac8b07431dc0a4f4ab7f04776bdca7f", {:callback_path => '/invites/yahoo/contact_callback'}
-  importer :hotmail, "client_id", "client_secret"
+  importer :yahoo, "dj0yJmk9TWFlM3FobzVWR1JVJmQ9WVdrOVZUbHVRalEzTTJVbWNHbzlNVFU0TWpneE56WXkmcz1jb25zdW1lcnNlY3JldCZ4PWMy", "13698743f814a9528e0387fc18875d24c4668a1d", {:callback_path => '/contacts/yahoo/callback'}
+  importer :hotmail, "00000000441006AC", "fg148d07CTyAdmL2zfDaxFBYZ65wQ2da"
 end
