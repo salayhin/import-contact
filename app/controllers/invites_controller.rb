113ec40d44f08851690137552308e6f7b10b2b8a
salayhin <salayhin@gmail.com>
2013-08-23 18:23:21 +0600

    debugging

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -2,6 +2,8 @@ class InvitesController < ApplicationController
 
   def index
     @contacts = request.env['omnicontacts.contacts']
+    @user = request.env['omnicontacts.user']
+    @contacts = request.env['omnicontacts.contacts']
     respond_to do |format|
       format.html
     end
