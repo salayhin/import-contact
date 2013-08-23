6dfaa3c95e3533266dd67f86c4b41078494ba2c2
salayhin <salayhin@gmail.com>
2013-08-23 12:20:41 +0600

    Import Contact Project Added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImportContact::Application.config.secret_token = '1c37945f1dbec48a28bf89cf1c05c02cf476ab5d0ff874dd5fe8f496cc145881504b2e18db3d2513a2629bbca11554cc3b7cc2606aee62781f91f7cf83fefa9f'
