f61e917969b37958e7f028b9cdfa900738b8a866
salayhin <salayhin@gmail.com>
2013-08-27 12:03:06 +0600

    debugging

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -14,7 +14,7 @@ class InvitesController < ApplicationController
     @user = request.env['omnicontacts.user']
     puts YAML::dump(@contacts)
     puts YAML::dump(@user)
-    puts "List of contacts of #{user[:name]} obtained from #{params[:importer]}:"
+    puts "List of contacts of #{@user[:name]} obtained from #{params[:importer]}:"
     @contacts.each do |contact|
       puts "Contact found: name => #{contact[:name]}, email => #{contact[:email]}"
     end
