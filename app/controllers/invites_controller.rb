385ee3c2731e03fdd69bc3e133cf70b005f79dc4
salayhin <salayhin@gmail.com>
2013-08-30 18:23:47 +0600

    Few modification done

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -2,18 +2,10 @@ class InvitesController < ApplicationController
 
   def index
     @contacts = request.env['omnicontacts.contacts']
-    puts YAML::dump(@contacts)
-    respond_to do |format|
-      format.html
-    end
   end
 
   def contacts_callback
     @contacts = request.env['omnicontacts.contacts']
-    #@user = request.env['omnicontacts.user']
-    puts YAML::dump(@contacts)
-    #puts YAML::dump(@user)
-    #puts "List of contacts of #{@user[:name]} obtained from #{params[:importer]}:"
 
   end
 
