86de2eed5b19617efc1ac977e8de9769e6fdfeb6
salayhin <salayhin@gmail.com>
2013-09-26 16:41:38 +0600

    Debugging

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,7 +1,7 @@
 class InvitesController < ApplicationController
 
   def index
-    session[:testing] = "this is a test"
+    session[:testing] = time.now
     @contacts = request.env['omnicontacts.contacts']
   end
 
@@ -12,22 +12,22 @@ class InvitesController < ApplicationController
 
   def invite_friends
     puts session[:testing]
-    @contacts = params[:email]
-
-    @contacts.each do |value|
-
-      if value.first.present?
-        @name = value.first
-      else
-        @name = value.last.split("@")[0].capitalize
-      end
-
-      @email = value.last
-      SendEmail.invite_friends(@name, @email).deliver
-
-    end
-
-    redirect_to({:action => :index}, :flash => {:enquiry => 'Your message has been sent'})
+    #@contacts = params[:email]
+    #
+    #@contacts.each do |value|
+    #
+    #  if value.first.present?
+    #    @name = value.first
+    #  else
+    #    @name = value.last.split("@")[0].capitalize
+    #  end
+    #
+    #  @email = value.last
+    #  SendEmail.invite_friends(@name, @email).deliver
+    #
+    #end
+    #
+    #redirect_to({:action => :index}, :flash => {:enquiry => 'Your message has been sent'})
     return true
 
     #Invite.send_email(@contacts)
